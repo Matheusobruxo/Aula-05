e18c74cb18f08ae7c789af40c2ef55fe96d013fa
matheus.hsilva76 <matheus.hsilva76@CAR0588294W10-1>
2023-06-05 16:39:13 -0300

    Adicionar arquivos de projeto.

---
+++ 05Junho/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Junho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age1;
+            Console.WriteLine("Digite a sua idade?");
+            age1 = int.Parse(Console.ReadLine());
+
+            if (age1 >= 18 )
+            {
+                Console.WriteLine("Me da dinheiro que eu te deixo dirigir\n\n");
+
+            }
+            else
+            {
+                Console.WriteLine("Espere mais alguns anos\n\n");
+            }
+            Main(null);
+        }
+    }
+}
